ccac565b312eea4117f10d002c977850fe79bd36
jeevan <jeevan@jeevandongre.net>
2016-02-16 16:19:18 +0530

    Added recipes to the main and with readme instructions.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Rsyslog - server.
 
 This recipe installs Rsys log on ubuntu 14.04 server and enables remote logging. 
 
+rsyslog.conf is configured to accept the traffic from the client machine on TCP port 514.
+
+If you are using AWS, please open TCP port 514 to send and recieve traffic using rsyslog
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+##Node Discovery
+node.set[:announce][:rsyslogserver] = true
+node.save
+
+execute 'rsyslogrepo'  do
+  command "sudo add-apt-repository ppa:adiscon/v8-stable"
+end
+
+execute "update" do
+  command "sudo apt-get update"
+end
+
+apt_package 'rsyslog' do
+  action :install          
+end	
+
+template "/etc/rsyslog.conf" do
+	source "rsyslog.conf.erb"
+	owner "syslog"
+	group "adm"
+	mode "0640"
+end	
+
+service "rsyslog" do
+	action :restart
+end	
