962691c0cad75390523cf98dc67e53b85ead24e0
jeevan <jeevan@jeevandongre.net>
2016-02-16 14:34:16 +0530

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'rsyslog-server'
+maintainer 'Jeevan Dongre'
+maintainer_email 'jeevan.dongre@gmail.com'
+license 'all_rights'
+description 'Installs/Configures rsyslog-server'
+long_description 'Installs/Configures rsyslog-server'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: rsyslog-server
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
